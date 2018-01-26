81bfa5065b037c1b84489215b9c402b3555826d7
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-01-26 13:12:38 +0530

    Create positiveornegative.c

---
+++ positiveornegative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value of a=");
+scanf("%d",&a);
+if (a==0)
+{
+printf("zero");
+}
+else if(a>0)
+{
+    printf("positive");
+}
+else
+{
+    printf("negative");
+}
+}
