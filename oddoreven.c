644e7dec9e6136a5cf96fdfb9fedea24a0cdd2a2
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-01-30 15:43:21 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
+
+
+
