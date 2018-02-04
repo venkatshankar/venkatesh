47cd14f6ef73ffa1931f1ede3942e13509ceb230
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-02-04 14:49:31 +0530

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    int *x=&a;
+    int **y=&x;
+    printf("%d\n",a);
+    printf("%d\n",x);
+    printf("%d\n",*x);
+    printf("%d\n",y);
+    printf("%d\n",*y);
+    printf("%d\n",**y);
+}
