be5160e4148709f787523dfade02e4afc7386211
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-02-04 12:11:49 +0530

    Update volumeusingfun.c

--- volumeusingfun.c
+++ volumeusingfun.c
@@ -2,12 +2,12 @@
 void main()
 {
     int r,h;
-    printf("enter the value of r and h:");
-    scanf("%d%d",&r,&h);
     volume(&r,&h);
 }
 int volume(int *a,int *b)
 {
+    printf("enter the value of r and h:");
+    scanf("%d%d",&(*a),&(*b));
     float c;
     c=(3.14*(*a)*(*a)*(*b));
     printf("volume=%f",c);
