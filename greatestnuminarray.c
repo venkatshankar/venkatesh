cccd82a20ce3840d030e866d1ea10d89f80b9444
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-02-27 16:24:04 +0530

    Create greatestnuminarray.c

---
+++ greatestnuminarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i,a[5],n,max=0;
+    printf("enter the no of elements:");
+    scanf("%d",&n);
+    printf("enter the array:");
+  
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }  max=a[1];
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}
