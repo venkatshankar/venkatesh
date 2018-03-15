40ce082e58575eea0fdc86802441af6949477e49
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-03-15 15:41:03 +0530

    Create numofcharinword.c

---
+++ numofcharinword.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[10],i,c=0;
+    printf("enter the character:");
+    scanf("%s",&s);
+    for(i=0;s[i]!=0;i++)
+    {
+        if(s[i]>'A' && s[i]<'z');
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
